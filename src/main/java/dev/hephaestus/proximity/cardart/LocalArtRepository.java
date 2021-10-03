9e5cb280ed105506d187da3967e48a21b7f6929a
gikkman <gikk@gikkman.com>
2021-10-03 13:02:26 +0200

    Move away from regex, use objects

--- src/main/java/dev/hephaestus/proximity/cardart/LocalArtRepository.java
+++ src/main/java/dev/hephaestus/proximity/cardart/LocalArtRepository.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,37 +13,25 @@ import dev.hephaestus.proximity.xml.RenderableCard;
 
 public class LocalArtRepository implements ArtRepository
 {
-	private static List<String> artPaths;
+	private static List<ArtFile> artPaths;
 
 	@Override public String findArt(RenderableCard card)
 	{
-		String name = card.getName();
-		String set = card.getAsString("set");
-		String setName = card.getAsString("set_name");
-		String artist = card.getAsString("artist");
-
-		// Regex for:  (SET)?/CARD_NAME( SET)?( ARTIST)?
-		// With groups   1       2        3        4
-		Pattern regex = Pattern.compile(
-			".+" +	                            // Allow for anything at the start
-			"("+set+"|"+setName+")?" +          // Check for set as directory name (Optional)
-			".+" +	                            // Allow for path separator (Optional, in case set was a directory)
-			"("+name+")" +                      // Check for the card name
-			"("+set+"|"+setName+")?" +          // Check for set in file name (Optional)
-			"( "+artist+")?" +                  // Check for artist in file name (Optional)
-			"\\..*", Pattern.CASE_INSENSITIVE); // Check for a dot, so we know there is a file extension straigh away
+		String name = LocalArtRepository.cleanString(card.getName());
+		String set = LocalArtRepository.cleanString(card.getAsString("set"));
+		String artist = LocalArtRepository.cleanString(card.getAsString("artist"));
 
 		// List of all paths to art files that contains the card name
 		Path artDirectory = Path.of("art");
 		String match = findAllArt(artDirectory)
 			.stream()
-			.map(regex::matcher)
-			.filter(Matcher::find)
-			.sorted(this::sortArt)
+			.filter(artFile -> name.equals(artFile.getCardName()) )
+			.sorted(this::orderArtByPrio)
+			.filter(artFile -> setFilter(artFile, set))
+			.filter(artFile -> artistFilter(artFile, artist))
 			.findFirst()
-			.map(Matcher::group)
+			.map(ArtFile::getPath)
 			.map(Path::of)
-			.map(artDirectory::resolve)
 			.map(Path::toAbsolutePath)
 			.map(Path::toFile)
 			.map(File::toURI)
@@ -52,57 +41,105 @@ public class LocalArtRepository implements ArtRepository
 		return match;
 	}
 
-	private int sortArt(Matcher l, Matcher r) {
-		// Regex for:  (SET)?/CARD_NAME( SET)?( ARTIST)?
-		// With groups   1       2        3        4
+	private int orderArtByPrio(ArtFile l, ArtFile r) {
 		/*  Card art matcher priority
-			1) SET/CARD_NAME (ARTIST)
-			2) SET/CARD_NAME
-			3) CARD_NAME (SET) (ARTIST)
-			4) CARD_NAME (SET)
-			5) CARD_NAME (ARTIST)
-			6) CARD_NAME
+			1) CARD_NAME [SET] (ARTIST)
+			2) CARD_NAME [SET]
+			3) CARD_NAME (ARTIST)
+			4) CARD_NAME
 		 */
-		if(l.group(1) != null && l.group(4) != null) return -1;
-		if(r.group(1) != null && r.group(4) != null) return 1;
-		if(l.group(1) != null) return -1;
-		if(r.group(1) != null) return 1;
-		if(l.group(3) != null && l.group(4) != null) return -1;
-		if(r.group(3) != null && r.group(4) != null) return 1;
-		if(l.group(3) != null) return -1;
-		if(r.group(3) != null) return 1;
-		if(l.group(4) != null) return -1;
-		if(r.group(4) != null) return 1;
+		if(l.hasSet() && l.hasArtist()) return -1;
+		if(r.hasSet() && r.hasArtist()) return 1;
+		if(l.hasSet()) return -1;
+		if(r.hasSet()) return 1;
+		if(l.hasArtist()) return -1;
+		if(r.hasArtist()) return 1;
 		return 0;
 	}
 
-	private static synchronized List<String> findAllArt(Path root) {
+	private boolean setFilter(ArtFile artFile, String set) {
+		if(artFile.hasSet())
+			return artFile.getSet().equals(set);
+		return true;
+	}
+
+	private boolean artistFilter(ArtFile artFile, String artist) {
+		if(artFile.hasArtist())
+			return artFile.getArtist().equals(artist);
+		return true;
+	}
+
+	private static synchronized List<ArtFile> findAllArt(Path root) {
 		if(LocalArtRepository.artPaths != null) return LocalArtRepository.artPaths;
-		List<String> artPaths = findFiles(root)
-			.stream()
-			.map(File::toPath)
-			.map(Path::toString)
-			.toList();
+		List<ArtFile> artPaths = findFiles(root);
 		LocalArtRepository.artPaths = artPaths;
 		return artPaths;
 	}
 
-	private static synchronized List<File> findFiles(Path directoryPath) {
-		File artDirectory = new File(directoryPath.toUri());
+	private static synchronized List<ArtFile> findFiles(Path directoryPath) {
+		File artDirectory = directoryPath.toFile();
 		File[] files = artDirectory.listFiles();
 		if(files == null) {
 			return new ArrayList<>();
 		}
-
-		var allFiles = new ArrayList<File>();
+		var allFiles = new ArrayList<ArtFile>();
 		for(File file : files) {
 			if(file.isDirectory()) {
 				allFiles.addAll(findFiles(file.toPath()));
 			}
 			else {
-				allFiles.add( file );
+				allFiles.add(ArtFile.of(file));
 			}
 		}
 		return allFiles;
+	}
+
+	private static String cleanString(String s) {
+		if(s == null) return null;
+		return s.replaceAll("[.,'-]", "").toLowerCase(Locale.ROOT).trim();
+	}
+
+	private static class ArtFile {
+		private static final Pattern cardNamePattern = Pattern.compile("^([^(\\[.])+"); //Match everything up to [ ( or .
+		private static final Pattern setNamePattern = Pattern.compile("\\[(.*?)]"); //Match everything between [ and ]
+		private static final Pattern artistPattern = Pattern.compile("\\((.*?)\\)"); //Match everything between ( and )
+		private final String cardName;
+		private final String set;
+		private final String artist;
+		private final String path;
+
+		private ArtFile(String cardName, String set, String artist, String path) {
+			this.cardName = cardName;
+			this.set = set;
+			this.artist = artist;
+			this.path = path;
+		}
+
+		private boolean hasSet() { return set != null; }
+		private boolean hasArtist() { return artist != null; }
+		private String getCardName() { return cardName; }
+		private String getSet() { return set; }
+		private String getArtist() { return artist; }
+		private String getPath() { return path; }
+
+		private static ArtFile of(File file) {
+			String parentDirectory = file.getParentFile().getName();
+			String fileName = file.getName();
+
+			Matcher cardNameMatches = cardNamePattern.matcher(fileName);
+			String cardName = cardNameMatches.find() ? cleanString(cardNameMatches.group()) : null;
+
+			Matcher artistMatches = artistPattern.matcher(fileName);
+			String artist = artistMatches.find() ? cleanString( artistMatches.group(1) ) : null;
+
+			Matcher setMatches = setNamePattern.matcher(fileName);
+			String set = setMatches.find()
+				? cleanString( setMatches.group() )
+				: parentDirectory.equals("art")
+				? null
+				: parentDirectory;
+
+			return new ArtFile(cardName, set, artist, file.getPath());
+		}
 	}
 }
