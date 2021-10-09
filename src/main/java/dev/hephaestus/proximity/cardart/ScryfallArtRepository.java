346b0c2f406884022cdcd9a9744bf64d31c9f77c
gikkman <gikk@gikkman.com>
2021-10-09 13:55:07 +0200

    Have online art resolver return wrapper input stream

--- src/main/java/dev/hephaestus/proximity/cardart/ScryfallArtRepository.java
+++ src/main/java/dev/hephaestus/proximity/cardart/ScryfallArtRepository.java
@@ -1,22 +1,39 @@
 package dev.hephaestus.proximity.cardart;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import dev.hephaestus.proximity.xml.RenderableCard;
 
-public class ScryfallArtRepository implements ArtRepository
+public class ScryfallArtRepository
 {
 	private static final Logger LOG = LogManager.getLogger("ScryfallArtRepository");
-	@Override public String findArt(RenderableCard card) {
+
+	public static Optional<ArtInputStreamWrapper> findArt(RenderableCard card) {
 		String name = card.getName();
 		String set = card.getSet();
 		LOG.debug("Fetching art uri from Scryfall for card {} [{}]", name, set);
 
 		String artUri = card.getAsString("image_uris", "art_crop");
-		if(artUri != null) LOG.debug("Scryfall art uri found for card {} [{}] at: {}", name, set, artUri);
-		else LOG.debug("No Scryfall art uri found for card {} [{}]", name, set);
-
-		return artUri;
+		InputStream inputStream = null;
+		if(artUri != null) {
+			LOG.debug("Scryfall art uri found for card {} [{}]. The uri was: {}", name, set, artUri);
+			try { inputStream = new URL(artUri).openStream(); }
+			catch (IOException e) {
+				LOG.error(e);
+				LOG.error("The Scryfall art uri found for card {} [{}] caused an error. The uri was: {}",
+					name, set, artUri);
+			}
+		}
+		else {
+			LOG.debug("No Scryfall art uri found for card {} [{}]", name, set);
+		}
+		return Optional.ofNullable( inputStream )
+			.map(is -> new ArtInputStreamWrapper(is, artUri));
 	}
 }
