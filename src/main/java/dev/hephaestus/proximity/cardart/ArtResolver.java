f9681be5a0203ec6669521e7c2b2160f6ad0c48a
gikkman <gikk@gikkman.com>
2021-10-09 13:54:28 +0200

    Make the ArtResolver return wrapped InputStream

--- src/main/java/dev/hephaestus/proximity/cardart/ArtResolver.java
+++ src/main/java/dev/hephaestus/proximity/cardart/ArtResolver.java
@@ -1,30 +1,15 @@
 package dev.hephaestus.proximity.cardart;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import dev.hephaestus.proximity.xml.RenderableCard;
 
 public class ArtResolver
 {
-	private static final Logger LOG = LogManager.getLogger("LocalArtRepository");
-	private final List<ArtRepository> artRepositories = new ArrayList<>();
-
-	public ArtResolver() {
-		artRepositories.add(new LocalArtRepository());
-		artRepositories.add(new ScryfallArtRepository());
-	}
-
-	public Optional<String> findArt(RenderableCard card) {
-		return artRepositories.stream()
-			.map(artRepository -> artRepository.findArt(card))
-			.filter(Objects::nonNull)
-			.peek(art -> LOG.debug("Choosing art at {}", art))
-			.findFirst();
+	public static ArtInputStreamWrapper findArt(RenderableCard card) {
+		return Optional.of(card)
+			.flatMap(LocalArtRepository::findArt)
+			.or(() -> ScryfallArtRepository.findArt(card))
+			.orElseGet(ArtInputStreamWrapper::nullInputStream);
 	}
 }
